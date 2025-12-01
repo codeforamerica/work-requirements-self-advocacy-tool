02201bceaa999dfe2cb6b0347ea7d1af23156428
Jenny Heath <jheath@codeforamerica.org>
2025-12-01 12:51:36 -0800

    Add ui components

--- app/controllers/language_preference_controller.rb
+++ app/controllers/language_preference_controller.rb
@@ -2,4 +2,11 @@ class LanguagePreferenceController < QuestionController
   def self.attributes_edited
     [:language_preference_written, :language_preference_spoken]
   end
+
+  helper_method :dropdown_options
+  def dropdown_options
+    [ OpenStruct.new(value: "english", label: "English"),
+      OpenStruct.new(value: "spanish", label: "Spanish"),
+      OpenStruct.new(value: "unfilled", label: "Gibberish") ]
+  end
 end
