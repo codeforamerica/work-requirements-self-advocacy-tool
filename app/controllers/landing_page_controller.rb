75d2a374771e5364f7a59f4a4fc57ea0aef2fcf1
Jenny Heath <jheath@codeforamerica.org>
2025-12-02 08:58:58 -0800

    lint

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -8,5 +8,4 @@ class LandingPageController < ApplicationController
     Screener.create
     redirect_to navigation_class.first.to_path_helper
   end
-
 end
--- app/controllers/language_preference_controller.rb
+++ app/controllers/language_preference_controller.rb
@@ -1,12 +1,13 @@
 class LanguagePreferenceController < QuestionController
+  helper_method :dropdown_options
+
   def self.attributes_edited
     [:language_preference_written, :language_preference_spoken]
   end
 
-  helper_method :dropdown_options
   def dropdown_options
-    [ OpenStruct.new(value: "english", label: "English"),
+    [OpenStruct.new(value: "english", label: "English"),
       OpenStruct.new(value: "spanish", label: "Spanish"),
-      OpenStruct.new(value: "unfilled", label: "Gibberish") ]
+      OpenStruct.new(value: "unfilled", label: "Gibberish")]
   end
 end
