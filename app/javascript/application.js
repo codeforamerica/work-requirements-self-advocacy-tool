5eceff635a4dacb9b251221715b129f2d8764a75
Jenny Heath <jheath@codeforamerica.org>
2025-12-16 14:37:04 -0800

    --wip-- [skip ci]
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+//= require cfa_styleguide_main
+
+// import "jquery-ui";
+// import 'jquery-rails'
+// import $ from "jquery"
+//
+// window.jQuery = $;
+// window.$ = $;
+//
+// import "./lib/honeycrisp";
