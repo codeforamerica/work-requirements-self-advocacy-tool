237e26da68599072df003b971f72889b1772e514
Jenny Heath <jheath@codeforamerica.org>
2025-12-16 15:16:53 -0800

    kevin figured out how to include the damn thing
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-//= require cfa_styleguide_main
-
-// import "jquery-ui";
-// import 'jquery-rails'
-// import $ from "jquery"
-//
-// window.jQuery = $;
-// window.$ = $;
-//
-// import "./lib/honeycrisp";
