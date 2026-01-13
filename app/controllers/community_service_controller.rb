6120d7ec84c946959f64f7350b57001182199c92
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-01-13 16:57:58 -0500

    lint

--- app/controllers/community_service_controller.rb
+++ app/controllers/community_service_controller.rb
@@ -1,6 +1,7 @@
 class CommunityServiceController < QuestionController
   include WrExemptionsConcern
+
   def self.attributes_edited
-    [:is_volunteer, :volunteering_hours, :volunteering_org_name ]
+    [:is_volunteer, :volunteering_hours, :volunteering_org_name]
   end
 end
