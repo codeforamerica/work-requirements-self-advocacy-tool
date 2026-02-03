2e54eeff648f9393d5dea6b280efed924f94d66e
Marc Perlman <mperlman@codeforamerica.org>
2026-02-03 14:59:34 -0600

    test

---
+++ spec/controllers/preventing_work_reason_controller_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe PreventingWorkReasonController, type: :controller do
+  describe "#update" do
+    it "persists the values to the current screener" do
+      screener = create(:screener)
+
+      params = {
+        preventing_work_additional_info: "This is my very good reason"
+      }
+
+      post :update, params: {screener: params}
+      screener.reload
+      expect(screener.preventing_work_additional_info).to eq "This is my very good reason"
+
+      params = {
+        preventing_work_additional_info: nil
+      }
+
+      post :update, params: {screener: params}
+      screener.reload
+      expect(screener.preventing_work_additional_info).to eq nil
+
+    end
+  end
+end
