f1bc738bdfeab00eedf601a64acaf6ec3df62c86
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-02-03 14:51:39 -0800

    lint and update gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,9 +23,9 @@ module ApplicationHelper
 
   def link_to_locale(locale, label, additional_attributes = {})
     link_to(label,
-            { locale: locale, params: request.query_parameters },
-            lang: locale,
-            id: "locale_switcher_#{locale}",
-            **additional_attributes).html_safe
+      {locale: locale, params: request.query_parameters},
+      lang: locale,
+      id: "locale_switcher_#{locale}",
+      **additional_attributes).html_safe
   end
 end
