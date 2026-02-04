1f3196bd4262793056122db7f764d54fca726a61
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2026-02-04 13:40:43 -0800

    WRSAT-273 Add alcohol treatment program screen (#92)

--- app/models/screener.rb
+++ app/models/screener.rb
@@ -22,6 +22,7 @@ class Screener < ApplicationRecord
   enum :is_in_work_training, {unfilled: 0, yes: 1, no: 2}, prefix: true
   enum :is_student, {unfilled: 0, yes: 1, no: 2}, prefix: true
   enum :is_migrant_farmworker, {unfilled: 0, yes: 1, no: 2}, prefix: true
+  enum :is_in_alcohol_treatment_program, {unfilled: 0, yes: 1, no: 2}, prefix: true
   enum :preventing_work_place_to_sleep, {unfilled: 0, yes: 1, no: 2}, prefix: true
   enum :preventing_work_drugs_alcohol, {unfilled: 0, yes: 1, no: 2}, prefix: true
   enum :preventing_work_domestic_violence, {unfilled: 0, yes: 1, no: 2}, prefix: true
@@ -32,7 +33,11 @@ class Screener < ApplicationRecord
   attr_writer :pregnancy_due_date_year, :pregnancy_due_date_month, :pregnancy_due_date_day
   normalizes :phone_number, with: ->(value) { Phonelib.parse(value, "US").national }
   before_validation :strip_email_and_confirmation
-  before_save :remove_pregnancy_attributes_if_no, :remove_volunteer_attributes_if_no, :remove_work_training_attributes_if_no, :remove_working_attributes_if_no
+  before_save :remove_pregnancy_attributes_if_no,
+    :remove_volunteer_attributes_if_no,
+    :remove_work_training_attributes_if_no,
+    :remove_working_attributes_if_no,
+    :remove_alcohol_treatment_program_attributes_if_no
 
   with_context :birth_date do
     validates :birth_date, presence: {message: I18n.t("validations.date_missing_or_invalid")}
@@ -162,6 +167,12 @@ class Screener < ApplicationRecord
     if is_in_work_training_no?
       self.work_training_hours = nil
       self.work_training_name = nil
+    end
+  end
+
+  def remove_alcohol_treatment_program_attributes_if_no
+    if is_in_alcohol_treatment_program_no?
+      self.alcohol_treatment_program_name = nil
     end
   end
 end
