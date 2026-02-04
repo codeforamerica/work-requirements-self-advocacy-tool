5e64887989d3ea34054a3db9f98b838562a20298
Marc Perlman <mperlman@codeforamerica.org>
2026-02-03 18:17:51 -0600

    fixing test

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -13,8 +13,13 @@ RSpec.describe I18n do
       "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
-  it "does not have unused keys" do
-    expect(unused_keys).to be_empty,
+  it "does not have unused keys other than 2 known" do
+    # there is 1 unused key across both locales, general.char_counter.remaining, that is
+    # pluralized, and i18n unused will not ignore them. The documentation is unclear on
+    # how this should work or not, but rather than fight with ignoring them for now, we
+    # know they will cause this to fail when checking if empty, so we can just declare
+    # these 2 known keys and if the number increases, that's the issue! 
+    expect(unused_keys.count).to eq(2),
       "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 
