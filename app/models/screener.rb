94cefa89b8e17cc01e991c0bc078cfabe6edeca0
Marc Perlman <mperlman@codeforamerica.org>
2026-02-11 09:05:03 -0600

    Removing validation on pregnancy yes/no

--- app/models/screener.rb
+++ app/models/screener.rb
@@ -60,7 +60,6 @@ class Screener < ApplicationRecord
   end
 
   with_context :pregnancy do
-    validates :is_pregnant, inclusion: {in: %w[yes no], message: I18n.t("validations.must_answer_yes_or_no")}
     validates :pregnancy_due_date, comparison: {greater_than: Date.current, message: I18n.t("validations.date_must_be_in_future")}, allow_blank: true
   end
 
