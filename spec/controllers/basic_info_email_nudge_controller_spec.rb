add95f18f839eab6b49e010afae21c7125bd9471
Marc Perlman <mperlman@codeforamerica.org>
2026-02-12 15:40:59 -0600

    Fixing tests

--- spec/controllers/basic_info_email_nudge_controller_spec.rb
+++ spec/controllers/basic_info_email_nudge_controller_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe BasicInfoEmailNudgeController, type: :controller do
   describe ".show?" do
     context "screener without email" do
       it "returns true" do
-        screener = create(:screener)
+        screener = create(:screener, is_working: "yes")
         expect(subject.class.show?(screener)).to eq true
       end
     end
 
     context "screener with email" do
       it "returns false " do
-        screener = create(:screener, email: "email@example.com")
+        screener = create(:screener, is_working: "yes", email: "email@example.com")
         expect(subject.class.show?(screener)).to eq false
       end
     end
