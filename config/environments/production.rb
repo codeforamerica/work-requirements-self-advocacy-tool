0ddb9d375283e0e81b6544a41fe146077becc4c9
Marc Perlman <mperlman@codeforamerica.org>
2026-02-24 16:23:33 -0600

    [WRSAT-329] Following vita-min pattern to define logging better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info").to_sym
-  config.log_tags = [:request_id]
+  config.log_tags = []
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
