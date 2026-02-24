493d79c03baff9a4bbbd43897d83039f5493720c
Marc Perlman <mperlman@codeforamerica.org>
2026-02-24 15:41:50 -0600

    [WRSAT-329] Following vita-min pattern to define logging better

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,23 +33,28 @@ Rails.application.configure do
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
-  # Log to STDOUT with the current request id as a default log tag.
-  config.log_tags = [:request_id]
-  
+  logger           = ActiveSupport::Logger.new($stdout)
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  logger.level     = Logger.const_get(ENV.fetch("RAILS_LOG_LEVEL", "INFO").upcase)
 
-  Rails.logger = ActiveSupport::Logger.new($stdout)
-  Rails.logger.formatter = proc do |severity, timestamp, _progname, message|
-    log_line =
+  logger.formatter = proc do |severity, timestamp, progname, message|
+    payload =
       if message.is_a? Hash
         # When messages go through lograge, they arrive here as a Hash
         message
       else
         { message: message, level: severity, time: timestamp }
       end
+
+    log_line = payload.merge(
+      level: severity,
+      time: timestamp.utc.iso8601(3)
+    )
     "#{log_line.to_json}\n"
   end
+
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  config.log_tags = [:request_id]
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,25 +1,18 @@
 Rails.application.configure do
-  # Lograge config
   config.lograge.enabled = true
 
-  # This specifies to log in JSON format
+  # Use Raw so it passes a Hash to the logger
   config.lograge.formatter = Lograge::Formatters::Raw.new
 
-  ## Disables log coloration
-  config.colorize_logging = false
+  # Prevent Rails default request logging
+  config.lograge.keep_original_rails_log = false
 
-  # Log to the same place as Rails logs
-  config.lograge.logger = Rails.logger
-
-  # This is useful if you want to log query parameters
   config.lograge.custom_options = lambda do |event|
     {
-      params: event.payload[:params]&.reject { |k| %w(controller action).include? k },
-      request_details: event.payload[:request_details],
-      level: event.payload[:level],
-      request_id: event.payload[:request_id] || event.payload[:headers]["action_dispatch.request_id"],
+      params: event.payload[:params]&.except("controller", "action"),
+      request_id: event.payload[:request_id],
       session_id: RequestStore.store[:session_id],
       screener_id: RequestStore.store[:screener_id]
-    }
+    }.compact
   end
 end
