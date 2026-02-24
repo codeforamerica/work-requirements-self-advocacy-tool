e1711e5548ae7ffefcb51d100dfd65a299ada8e2
Marc Perlman <mperlman@codeforamerica.org>
2026-02-24 12:28:22 -0600

    Adding tests

--- app/models/screener.rb
+++ app/models/screener.rb
@@ -66,7 +66,7 @@ class Screener < ApplicationRecord
     preventing_work_other
   ].freeze
 
-  # TODO: Homeschooling and 5 year work eligibity for NC
+  # TODO: Homeschooling and 5 year work eligibility for NC
   def exempt_from_work_rules?
     return true if age_qualified?
 
