0fddbb25bf74103341050522018ef5a05e17726d
Marc Perlman <mperlman@codeforamerica.org>
2026-03-04 13:20:14 -0600

    Do not try to set trace_id for noop

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -6,13 +6,14 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
 
   config.lograge.custom_options = lambda do |event|
+    current_span = OpenTelemetry::Trace.current_span
     {
       request_id: event.payload[:headers]&.[]("action_dispatch.request_id"),
       session_id: RequestStore.store[:session_id],
       screener_id: RequestStore.store[:screener_id],
       level: event.payload[:level] || "INFO",
       time: Time.now.utc.iso8601(3),
-      trace_id: OpenTelemetry::Trace.current_span.context.hex
-    }.compact
+      trace_id: current_span.context.valid? ? current_span.context.hex : nil
+    }.compact 
   end
 end
