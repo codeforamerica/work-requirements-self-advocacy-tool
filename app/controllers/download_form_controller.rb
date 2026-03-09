2477ef574c3b22aa1a08b414b8fca4599e25e7b0
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-03-09 16:58:48 -0400

    remove flash notice since the flow will no longer need it

--- app/controllers/download_form_controller.rb
+++ app/controllers/download_form_controller.rb
@@ -6,7 +6,6 @@ class DownloadFormController < QuestionController
   def email_pdf
     outgoing_email = OutgoingEmail.create(screener: current_screener)
     SendOutgoingEmailJob.perform_later(outgoing_email.id)
-    flash[:notice] = I18n.t("views.download_form.edit.notice")
     redirect_to download_form_path
   end
 end
