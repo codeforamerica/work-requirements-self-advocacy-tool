f3d430faa2aee120dfea8b653cb6124e02418c65
James Armes <jarmes@codeforamerica.org>
2026-03-10 13:01:13 -0400

    fix: Linting errors.

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -4,7 +4,7 @@ ActiveSupport::Notifications.subscribe("polling.solid_queue") do |_name, start, 
   tracer = OpenTelemetry.tracer_provider.tracer("solid_queue")
   span = tracer.start_span(
     "polling solid_queue",
-    attributes: { "polling.solid_queue" => "true" },
+    attributes: {"polling.solid_queue" => "true"},
     start_timestamp: start
   )
   span.finish(end_timestamp: finish)
