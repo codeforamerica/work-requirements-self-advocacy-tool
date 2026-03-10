2f9fa26ccf5ed72aee78ee7ce205e892a8b4d310
James Armes <jarmes@codeforamerica.org>
2026-03-10 18:01:03 -0400

    fix: Use the proper, current span.

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-ActiveSupport::Notifications.subscribe("polling.solid_queue") do |_name, start, finish, _id, _payload|
-  tracer = OpenTelemetry.tracer_provider.tracer("solid_queue")
-  span = tracer.start_span(
-    "polling solid_queue",
-    attributes: {"polling.solid_queue" => "true"},
-    start_timestamp: start
-  )
-  span.finish(end_timestamp: finish)
+ActiveSupport::Notifications.subscribe("polling.solid_queue") do
+  OpenTelemetry::Trace.current_span.set_attribute("polling.solid_queue", "true")
 end
+
