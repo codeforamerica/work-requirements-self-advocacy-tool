fe71f0e3a73879fccc5e4731f9bc3d97f5449388
James Armes <jarmes@codeforamerica.org>
2026-03-13 13:24:53 -0400

    try removing tagged logger configuration.

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,8 @@ module WorkRequirementsSelfAdvocacyTool
     config.i18n.fallbacks = [I18n.default_locale]
     config.i18n.available_locales = [:en, :es]
 
-    config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout))
     # Change to "debug" to log everything (including potentially personally-identifiable information!)
     config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info").to_sym
-    config.log_tags = []
 
     # If gem "image_processing", "~> 1.2" is added/uncommented out in Gemfile, this should be removed
     # Otherwise, this prevents the application from unnecessarily logging:
