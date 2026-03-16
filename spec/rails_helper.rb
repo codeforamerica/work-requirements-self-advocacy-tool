25999e86e594fe2463d1ac5d3ad8ec3acfa99257
Jenny Heath <jheath@codeforamerica.org>
2026-03-16 10:46:05 -0700

    Make the tests stop printing a bunch of stuff

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
+ENV["OTEL_SDK_DISABLED"] ||= "true"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -35,6 +36,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  SemanticLogger.default_level = :fatal
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join("spec/fixtures")
