a4118a4af12508cf6cdf82e4c86e45f1868d45e4
Marc Perlman <mperlman@codeforamerica.org>
2026-03-18 14:11:00 -0500

    Cleanup

--- app/controllers/out_of_state_controller.rb
+++ app/controllers/out_of_state_controller.rb
@@ -3,7 +3,30 @@ class OutOfStateController < QuestionController
     false
   end
 
+  helper_method :not_listed?, :county_not_supported?, :county
+
+  def not_listed?
+    self.class.not_listed?(@current_screener)
+  end
+
+  def county_not_supported?
+    self.class.county_not_supported?(@current_screener)
+  end
+
+  def county
+    LocationData::Counties.get(@current_screener.state, @current_screener.county)
+  end
+
+  def self.not_listed?(screener)
+    screener.state == LocationData::States::NOT_LISTED
+  end
+
+  def self.county_not_supported?(screener)
+    county = LocationData::Counties.get(screener.state, screener.county)
+    !!screener.county && county.present? && !county[:is_supported]
+  end
+
   def self.show?(screener, item_index: nil)
-    screener.state == LocationData::States::NOT_LISTED || (!!screener.county && !LocationData::Counties.get(screener.state, screener.county)[:is_supported])
+    self.not_listed?(screener) || self.county_not_supported?(screener)
   end
 end
