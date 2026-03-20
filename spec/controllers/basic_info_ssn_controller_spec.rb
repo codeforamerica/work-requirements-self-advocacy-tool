31fc68fa8c131f44d9f1c6e07dc37ef389909191
Marc Perlman <mperlman@codeforamerica.org>
2026-03-20 14:32:03 -0500

    WRSAT-173

---
+++ spec/controllers/basic_info_ssn_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe BasicInfoSsnController, type: :controller do
+  describe "#update" do
+    it "saves the last 4 digits of the ssn and redirects to the next step" do
+      screener = create(:screener)
+      sign_in screener
+
+      post :update, params: {screener: {ssn: "4567"}}
+
+      expect(response).to redirect_to subject.next_path
+      expect(screener.reload.ssn).to eq "4567"
+    end
+  end
+end
