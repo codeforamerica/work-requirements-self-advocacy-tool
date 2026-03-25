3d08aa85bf5f20c592ecfc7ca0220a9e08c312ea
Marc Perlman <mperlman@codeforamerica.org>
2026-03-25 17:06:05 -0500

    more

--- app/controllers/basic_info_email_controller.rb
+++ app/controllers/basic_info_email_controller.rb
@@ -4,20 +4,14 @@ class BasicInfoEmailController < QuestionController
   before_action :set_from_download_form
 
   def self.attributes_edited
-    [:email, :email_confirmation, :from_download_form]
-  end
-
-  # allow the hidden field to come through
-  def form_params(model)
-    permitted = params.require(model.class.params_key)
-      .permit(*self.class.attributes_edited)
-
-    # convert string -> boolean
-    model.from_download_form = ActiveModel::Type::Boolean.new.cast(permitted[:from_download_form])
-    permitted.except(:from_download_form)
+    [:email, :email_confirmation, :from_download_form, :return_to_review]
   end
 
   def set_from_download_form
-    current_screener.from_download_form ||= params[:source] == "download-form"
+    current_screener.from_download_form ||= params[:from_download_form] == "true"
+  end
+
+  def review_controller
+    DownloadFormController
   end
 end
