b8354745203d25d6b4d0932aff865c475687de2f
Marc Perlman <mperlman@codeforamerica.org>
2026-03-25 16:26:51 -0500

    trap the user on the email page

--- app/controllers/basic_info_email_controller.rb
+++ app/controllers/basic_info_email_controller.rb
@@ -13,7 +13,7 @@ class BasicInfoEmailController < QuestionController
   # allow the hidden field to come through
   def form_params(model)
     permitted = params.require(model.class.params_key)
-                      .permit(*self.class.attributes_edited)
+      .permit(*self.class.attributes_edited)
 
     # convert string -> boolean
     model.from_download_form = ActiveModel::Type::Boolean.new.cast(permitted[:from_download_form])
