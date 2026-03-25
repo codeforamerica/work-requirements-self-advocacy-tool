ba202c1807b07e37612fbce4d18ab22a85598c47
Marc Perlman <mperlman@codeforamerica.org>
2026-03-25 17:33:36 -0500

    Fixing test

--- spec/controllers/download_form_controller_spec.rb
+++ spec/controllers/download_form_controller_spec.rb
@@ -1,11 +1,23 @@
 require "rails_helper"
 
 RSpec.describe DownloadFormController, type: :controller do
-  describe "#email_pdf" do
-    let!(:screener) { create(:screener) }
+  describe "#edit" do
+    let(:screener) { create(:screener, email: "hi@example.com") }
 
-    it "creates an outgoing email sends the email" do
-      expect { get :email_pdf }.to have_enqueued_job(SendOutgoingEmailJob)
+    before { sign_in screener }
+
+    it "enqueues a SendOutgoingEmailJob if the screener has an email" do
+      expect {
+        get :edit
+      }.to have_enqueued_job(SendOutgoingEmailJob).with(kind_of(Integer))
+    end
+
+    it "does not enqueue a job if the screener has no email" do
+      screener.update!(email: nil)
+
+      expect {
+        get :edit
+      }.not_to have_enqueued_job(SendOutgoingEmailJob)
     end
   end
 end
