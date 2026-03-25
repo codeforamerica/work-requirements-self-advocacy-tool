8f60be1d496deadd407da8617248a1081ff400ad
Marc Perlman <mperlman@codeforamerica.org>
2026-03-25 16:32:46 -0500

    trap the user on the email page

--- app/controllers/basic_info_email_controller.rb
+++ app/controllers/basic_info_email_controller.rb
@@ -1,13 +1,10 @@
 class BasicInfoEmailController < QuestionController
   include BasicInfoConcern
 
+  before_action :set_from_download_form
+
   def self.attributes_edited
     [:email, :email_confirmation, :from_download_form]
-  end
-
-  def edit
-    super
-    @model.from_download_form ||= params[:source] == "download-form"
   end
 
   # allow the hidden field to come through
@@ -18,5 +15,9 @@ class BasicInfoEmailController < QuestionController
     # convert string -> boolean
     model.from_download_form = ActiveModel::Type::Boolean.new.cast(permitted[:from_download_form])
     permitted.except(:from_download_form)
+  end
+
+  def set_from_download_form
+    current_screener.from_download_form ||= params[:source] == "download-form"
   end
 end
