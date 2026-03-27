7a6dd72e199cc177f9e991bcf5b39b549bd19acf
Marc P <mperlman@codeforamerica.org>
2026-03-27 14:48:30 -0500

    [WRSAT-464] Adding birthday persistence and validation to basic info details (#178)

--- spec/models/screener_spec.rb
+++ spec/models/screener_spec.rb
@@ -60,13 +60,14 @@ RSpec.describe Screener, type: :model do
     end
 
     context "with_context :basic_info_details" do
-      it "requires first name and last name" do
+      it "requires first name, last name, and birthday" do
         screener = Screener.new(first_name: nil, last_name: nil)
         screener.valid?(:basic_info_details)
 
         expect(screener.errors).to match_array [
           "First name can't be blank",
-          "Last name can't be blank"
+          "Last name can't be blank",
+          "Birth date Date is missing or invalid"
         ]
       end
 
