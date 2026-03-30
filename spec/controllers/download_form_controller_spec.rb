db246a41694d6c0af44f1be69b8a67f4d5662539
Marc Perlman <mperlman@codeforamerica.org>
2026-03-30 15:03:56 -0500

    Adding test

--- spec/controllers/download_form_controller_spec.rb
+++ spec/controllers/download_form_controller_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe DownloadFormController, type: :controller do
       }.not_to have_enqueued_job(SendOutgoingEmailJob)
     end
   end
+
+  describe ".show?" do
+    controller(DownloadFormController) do
+      def super_show_result
+        true
+      end
+
+      def show?(screener)
+        !!(screener&.exempt_from_work_rules? && super_show_result)
+      end
+    end
+
+    subject { controller.show?(screener) }
+
+    def set_super_result(value)
+      allow(controller).to receive(:super_show_result).and_return(value)
+    end
+
+    let(:screener) { instance_double("Screener") }
+
+    it_behaves_like "show? with work rules exemption"
+  end
 end
---
+++ spec/support/shared_examples/show_with_exemption.rb
@@ -0,0 +1,37 @@
+RSpec.shared_examples "show? with work rules exemption" do
+  context "when screener is nil" do
+    let(:screener) { nil }
+
+    it "returns false" do
+      expect(subject).to eq false
+    end
+  end
+
+  context "when screener is not exempt from work rules" do
+    let(:screener) { instance_double("Screener", exempt_from_work_rules?: false) }
+
+    it "returns false even if super would return true" do
+      set_super_result(true)
+      expect(subject).to eq false
+    end
+
+    it "returns false if super would return false" do
+      set_super_result(false)
+      expect(subject).to eq false
+    end
+  end
+
+  context "when screener is exempt from work rules" do
+    let(:screener) { instance_double("Screener", exempt_from_work_rules?: true) }
+
+    it "returns true if super returns true" do
+      set_super_result(true)
+      expect(subject).to eq true
+    end
+
+    it "returns false if super returns false" do
+      set_super_result(false)
+      expect(subject).to eq false
+    end
+  end
+end
