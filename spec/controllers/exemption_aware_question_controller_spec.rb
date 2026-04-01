35fbd40193327144bdb6363f6c76984e762d3a74
Marc Perlman <mperlman@codeforamerica.org>
2026-04-01 15:41:02 -0500

    Another test

---
+++ spec/controllers/exemption_aware_question_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe ExemptionAwareQuestionController, type: :controller do
+  # Minimal subclass so Rails can instantiate the controller
+  controller(ExemptionAwareQuestionController) do
+    def index
+      head :ok
+    end
+  end
+
+  let(:screener) { create(:screener) }
+
+  describe ".show?" do
+    subject { controller.class.show?(screener) }
+
+    context "when screener is exempt from work rules" do
+      before { screener.update!(preventing_work_place_to_sleep: "yes") }
+
+      # post :update, params: {screener: {email: "", email_confirmation: ""}, from_download_form: "true"}
+
+      it "returns true if parent allows showing" do
+        expect(subject).to eq(true)
+      end
+    end
+
+    context "when screener is not exempt from work rules" do
+      before { screener.update!(preventing_work_place_to_sleep: "no") }
+
+      it "returns false" do
+        expect(subject).to eq(false)
+      end
+    end
+  end
+end
