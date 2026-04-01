9d54962010a9636eff5062cdcbf5e3803f2b19fc
Marc Perlman <mperlman@codeforamerica.org>
2026-04-01 11:02:11 -0500

    test

---
+++ spec/controllers/work_rules_apply_unmet_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe WorkRulesApplyUnmetController, type: :controller do
+  describe ".show?" do
+    context "screener without any exemptions" do
+      it "returns true" do
+        screener = create(:screener, is_working: "no")
+        expect(subject.class.show?(screener)).to eq true
+      end
+    end
+
+    context "screener with any exemptions" do
+      it "returns true" do
+        screener = create(:screener, is_working: "yes", working_hours: "35")
+        expect(subject.class.show?(screener)).to eq false
+      end
+    end
+  end
+end
