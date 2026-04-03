8898ebd97d80402be10ce7f03958da69b7a86b3f
James Armes <jarmes@codeforamerica.org>
2026-04-03 14:20:35 -0400

    fix: Capture and log exceptions from job threads.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,21 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  # Make sure to capture and record any exceptions that occur in jobs.
+  rescue_from(Exception) do |error|
+    span = OpenTelemetry::Trace.current_span
+    span.status = OpenTelemetry::Trace::Status.error(error.message)
+    span.record_exception(
+      error,
+      attributes: { "job.class" => self.class.name, "job.id" => job_id }
+    )
+
+    Rails.logger.error("Job failed", job_class: self.class.name, job_id: job_id,
+                       error: error.message, error_class: error.class.name,
+                       backtrace: error.backtrace&.first(5))
+
+    # Re-raise the exception to be handled by the job framework (SolidQueue).
+    raise error
+  end
 end
---
+++ config/initializers/solid_queue.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Surface SolidQueue thread-level errors (worker/dispatcher crashes) to logs and
+# traces. Job-level errors are handled in ApplicationJob#rescue_from.
+SolidQueue.on_thread_error = lambda do |error|
+  span = OpenTelemetry::Trace.current_span
+  span.status = OpenTelemetry::Trace::Status.error(error.message)
+  span.record_exception(error)
+
+  Rails.logger.error("SolidQueue thread error", error: error.message,
+                     error_class: error.class.name,
+                     backtrace: error.backtrace&.first(5))
+end
