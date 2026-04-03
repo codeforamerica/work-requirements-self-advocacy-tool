aa7773aa992538a450436d523cb9562b625454cb
Marc Perlman <mperlman@codeforamerica.org>
2026-04-03 14:49:01 -0500

    [WRSAT-480] Make email logic safer

--- app/mailers/screener_mailer.rb
+++ app/mailers/screener_mailer.rb
@@ -3,7 +3,8 @@ class ScreenerMailer < ApplicationMailer
 
   def send_screener_results(outgoing_email:)
     @screener = outgoing_email.screener
-    @county_data = LocationData::Counties.get(@screener.state, @screener.county)
+    @state = @screener.state
+    @county = @screener.county
     attachments.inline["gbh_email_header.png"] = File.binread(Rails.root.join("app/assets/images/gbh_email_header.png"))
     mail(to: @screener.email, subject: I18n.t("views.screener_mailer.send_screener_results.subject"))
   end
