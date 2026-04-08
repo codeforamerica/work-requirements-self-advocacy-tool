dd0f3cc80cc0e4cbd54400cd572ac9847ef07d04
Jenny Heath <jheath@codeforamerica.org>
2026-04-08 11:33:38 -0700

    Add conditional content to email

--- test/mailers/previews/screener_mailer_preview.rb
+++ test/mailers/previews/screener_mailer_preview.rb
@@ -7,7 +7,14 @@ class ScreenerMailerPreview < ActionMailer::Preview
       last_name: "Ham",
       state: "NC",
       county: "Burke County",
-      confirmation_code: "123ABC"
+      confirmation_code: "123ABC",
+      is_student: "yes",
+      preventing_work_drugs_alcohol: "yes",
+      receiving_benefits_ssdi: "yes",
+      receiving_benefits_ssi: "yes",
+      receiving_benefits_veterans_disability: "yes",
+      receiving_benefits_other: "yes",
+      is_in_alcohol_treatment_program: "yes"
     )
     outgoing_email = FactoryBot.build(:outgoing_email, screener: screener)
     ScreenerMailer.send_screener_results(outgoing_email: outgoing_email)
