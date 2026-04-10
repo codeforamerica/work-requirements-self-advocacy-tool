5c0e6be4c38373265a8a511ec8a82f421ad311b1
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2026-04-10 10:19:08 -0700

    WRSAT-497 Add AuthenticatedConcern, test all of the controllers (#209)
    * handles the case where someone's session times out and they see a 500 when they refresh or click to another page

--- spec/controllers/basic_info_email_controller_spec.rb
+++ spec/controllers/basic_info_email_controller_spec.rb
@@ -1,87 +1,95 @@
 require "rails_helper"
 
 RSpec.describe BasicInfoEmailController, type: :controller do
-  let(:screener) { create(:screener) }
-  before { sign_in screener }
-
-  describe "#update" do
-    context "when email has extra spaces or capitalization" do
-      it "saves the normalized lowercase email" do
-        params = {
-          email: "anisHa@codeforamerica.org ",
-          email_confirmation: " anisha@codeforamerica.org"
-        }
-
-        post :update, params: {screener: params}
-        expect(response).to redirect_to(subject.next_path)
-        expect(screener.reload.email).to eq "anisha@codeforamerica.org"
-      end
-    end
-
-    context "when email is invalid" do
-      it "does not save and renders the form with errors" do
-        params = {
-          email: "invalid-email",
-          email_confirmation: "invalid-email"
-        }
-
-        post :update, params: {screener: params}
-        expect(response).to render_template(:edit)
-
-        expect(assigns(:model).errors[:email]).to eq [I18n.t("validations.email_invalid")]
-      end
-    end
-
-    context "when email and confirmation do not match" do
-      it "does not save and renders the form with errors" do
-        params = {
-          email: "test@example.com",
-          email_confirmation: "different@example.com"
-        }
-
-        post :update, params: {screener: params}
-        expect(response).to render_template(:edit)
-        expect(assigns(:model).errors[:email_confirmation]).to eq [I18n.t("validations.email_must_match")]
-      end
-    end
-
-    context "when from_download_form param is present" do
-      it "requires email presence" do
-        post :update, params: {screener: {email: "", email_confirmation: ""}, from_download_form: "true"}
-
-        expect(response).to render_template(:edit)
-        expect(assigns(:model).errors[:email]).to eq [I18n.t("validations.email_invalid")]
-      end
-    end
-
-    context "when return_to_review param is present" do
-      it "preserves the param for the controller flow" do
-        post :update,
-          params: {
-            screener: {email: "test@example.com", email_confirmation: "test@example.com"},
-            return_to_review: "y"
-          }
-
-        expect(response).to redirect_to(subject.next_path)
-      end
-    end
-
-    context "when no changes are made" do
-      it "keeps the existing email intact" do
-        screener.update!(email: "existing@example.com")
-        post :update, params: {screener: {email: "", email_confirmation: ""}, from_download_form: "true"}
-
-        expect(screener.reload.email).to eq "existing@example.com"
-      end
-    end
+  describe "#edit" do
+    it_behaves_like :session_must_be_active_for_this_get_action, action: :edit
   end
 
-  it "adds the confirmation code" do
-    expect(screener.confirmation_code).to be_nil
+  describe "#update" do
+    it_behaves_like :session_must_be_active_for_this_post_action, action: :edit
 
-    post :update, params: {screener: {email: "boop@example.com ", email_confirmation: "boop@example.com"}}
+    context "with signed in screener" do
+      let(:screener) { create(:screener) }
+      before { sign_in screener }
 
-    expect(screener.reload.confirmation_code).to be_present
+      context "when email has extra spaces or capitalization" do
+        it "saves the normalized lowercase email" do
+          params = {
+            email: "anisHa@codeforamerica.org ",
+            email_confirmation: " anisha@codeforamerica.org"
+          }
+
+          post :update, params: {screener: params}
+          expect(response).to redirect_to(subject.next_path)
+          expect(screener.reload.email).to eq "anisha@codeforamerica.org"
+        end
+      end
+
+      context "when email is invalid" do
+        it "does not save and renders the form with errors" do
+          params = {
+            email: "invalid-email",
+            email_confirmation: "invalid-email"
+          }
+
+          post :update, params: {screener: params}
+          expect(response).to render_template(:edit)
+
+          expect(assigns(:model).errors[:email]).to eq [I18n.t("validations.email_invalid")]
+        end
+      end
+
+      context "when email and confirmation do not match" do
+        it "does not save and renders the form with errors" do
+          params = {
+            email: "test@example.com",
+            email_confirmation: "different@example.com"
+          }
+
+          post :update, params: {screener: params}
+          expect(response).to render_template(:edit)
+          expect(assigns(:model).errors[:email_confirmation]).to eq [I18n.t("validations.email_must_match")]
+        end
+      end
+
+      context "when from_download_form param is present" do
+        it "requires email presence" do
+          post :update, params: {screener: {email: "", email_confirmation: ""}, from_download_form: "true"}
+
+          expect(response).to render_template(:edit)
+          expect(assigns(:model).errors[:email]).to eq [I18n.t("validations.email_invalid")]
+        end
+      end
+
+      context "when return_to_review param is present" do
+        it "preserves the param for the controller flow" do
+          post :update,
+            params: {
+              screener: {email: "test@example.com", email_confirmation: "test@example.com"},
+              return_to_review: "y"
+            }
+
+          expect(response).to redirect_to(subject.next_path)
+        end
+      end
+
+      context "when no changes are made" do
+        it "keeps the existing email intact" do
+          screener.update!(email: "existing@example.com")
+          post :update, params: {screener: {email: "", email_confirmation: ""}, from_download_form: "true"}
+
+          expect(screener.reload.email).to eq "existing@example.com"
+        end
+      end
+
+      it "adds the confirmation code" do
+        expect(screener.confirmation_code).to be_nil
+
+        post :update, params: {screener: {email: "boop@example.com ", email_confirmation: "boop@example.com"}}
+
+        expect(screener.reload.confirmation_code).to be_present
+      end
+    end
   end
 
   describe ".show?" do
