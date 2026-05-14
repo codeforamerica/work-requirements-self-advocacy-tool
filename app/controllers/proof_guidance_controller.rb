46169b4a65dcb5e45a3d42570051c63d4f0733e8
Marc Perlman <mperlman@codeforamerica.org>
2026-05-14 16:55:46 -0500

    [WRSAT-582]

--- app/controllers/proof_guidance_controller.rb
+++ app/controllers/proof_guidance_controller.rb
@@ -2,4 +2,10 @@ class ProofGuidanceController < ExemptionAwareQuestionController
   def show_progress_bar
     false
   end
+
+  helper_method :display_american_indian_guidance?
+
+  def display_american_indian_guidance?
+    @current_screener.state != "NC" && @current_screener.is_american_indian_yes?
+  end
 end
