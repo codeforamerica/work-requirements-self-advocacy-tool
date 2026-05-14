ac2f2035353e128ed6477b760f47a11ec397bf07
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-05-14 13:54:38 -0400

    move download form translation logic to controller

--- app/controllers/download_form_controller.rb
+++ app/controllers/download_form_controller.rb
@@ -1,9 +1,17 @@
 class DownloadFormController < ExemptionAwareQuestionController
+  before_action :email_pdf
+  helper_method :state_epass_html
   def show_progress_bar
     false
   end
 
-  before_action :email_pdf
+  def state_epass_html
+    if @current_screener.state == LocationData::States::NORTH_CAROLINA
+      I18n.t("views.download_form.edit.epass_nc_html")
+    else
+      I18n.t("views.download_form.edit.epass_de_html")
+    end
+  end
 
   def email_pdf
     return if current_screener.email.blank?
