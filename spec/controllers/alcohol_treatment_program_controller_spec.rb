258690a09d83a452434d04b0a5a3f99223913263
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-05-15 18:55:54 -0400

    update DE alcohol screen

--- spec/controllers/alcohol_treatment_program_controller_spec.rb
+++ spec/controllers/alcohol_treatment_program_controller_spec.rb
@@ -27,4 +27,23 @@ RSpec.describe AlcoholTreatmentProgramController, type: :controller do
   describe ".show?" do
     it_behaves_like "a show method that considers age exemption"
   end
+
+  describe "#state_notice_text" do
+    let(:screener) { create(:screener, state: state) }
+    context "when NC" do
+      let(:state) { LocationData::States::NORTH_CAROLINA }
+      it "returns the translation for NC" do
+        controller.instance_variable_set(:@current_screener, screener)
+        expect(controller.state_notice_text).to eq(I18n.t("views.alcohol_treatment_program.edit.notice_text_nc"))
+      end
+    end
+
+    context "when DE" do
+      let(:state) {  create(:screener, state: LocationData::States::DELAWARE) }
+      it "returns the translation for DE" do
+        controller.instance_variable_set(:@current_screener, screener)
+        expect(controller.state_notice_text).to eq(I18n.t("views.alcohol_treatment_program.edit.notice_text_de"))
+      end
+    end
+  end
 end
