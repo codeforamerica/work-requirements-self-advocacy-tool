ec2105e370d2528b25ee95282a7b84a831eafbd7
Jenny Heath <jheath@codeforamerica.org>
2026-05-19 09:24:55 -0700

    wip

--- test/mailers/previews/screener_mailer_preview.rb
+++ test/mailers/previews/screener_mailer_preview.rb
@@ -41,4 +41,25 @@ class ScreenerMailerPreview < ActionMailer::Preview
     outgoing_email = FactoryBot.build(:outgoing_email, screener: screener)
     ScreenerMailer.send_screener_results(outgoing_email: outgoing_email)
   end
+
+  def send_screener_results_de_special_geo
+    screener = FactoryBot.build(
+      :screener,
+      email: "preview@example.com",
+      first_name: "Kat",
+      last_name: "Spam",
+      state: "DE",
+      zip_code: "19720",
+      confirmation_code: "2345DEF",
+      is_student: "yes",
+      preventing_work_drugs_alcohol: "yes",
+      receiving_benefits_ssdi: "yes",
+      receiving_benefits_ssi: "yes",
+      receiving_benefits_veterans_disability: "yes",
+      receiving_benefits_other: "yes",
+      is_in_alcohol_treatment_program: "yes"
+    )
+    outgoing_email = FactoryBot.build(:outgoing_email, screener: screener)
+    ScreenerMailer.send_screener_results(outgoing_email: outgoing_email)
+  end
 end
