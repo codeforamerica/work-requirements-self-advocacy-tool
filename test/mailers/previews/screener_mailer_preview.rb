27a52869f4d6efea4cd48431aad87fcf71d5bdbd
Jenny Heath <jheath@codeforamerica.org>
2026-05-18 17:51:45 -0700

    wip: updates to email

--- test/mailers/previews/screener_mailer_preview.rb
+++ test/mailers/previews/screener_mailer_preview.rb
@@ -1,5 +1,5 @@
 class ScreenerMailerPreview < ActionMailer::Preview
-  def send_screener_results
+  def send_screener_results_nc
     screener = FactoryBot.build(
       :screener,
       email: "preview@example.com",
@@ -16,6 +16,27 @@ class ScreenerMailerPreview < ActionMailer::Preview
       receiving_benefits_other: "yes",
       is_in_alcohol_treatment_program: "yes",
       nc_screener: FactoryBot.build(:nc_screener)
+    )
+    outgoing_email = FactoryBot.build(:outgoing_email, screener: screener)
+    ScreenerMailer.send_screener_results(outgoing_email: outgoing_email)
+  end
+
+  def send_screener_results_de
+    screener = FactoryBot.build(
+      :screener,
+      email: "preview@example.com",
+      first_name: "Cat",
+      last_name: "Spam",
+      state: "DE",
+      zip_code: "19735",
+      confirmation_code: "2345DEF",
+      is_student: "yes",
+      preventing_work_drugs_alcohol: "yes",
+      receiving_benefits_ssdi: "yes",
+      receiving_benefits_ssi: "yes",
+      receiving_benefits_veterans_disability: "yes",
+      receiving_benefits_other: "yes",
+      is_in_alcohol_treatment_program: "yes"
     )
     outgoing_email = FactoryBot.build(:outgoing_email, screener: screener)
     ScreenerMailer.send_screener_results(outgoing_email: outgoing_email)
