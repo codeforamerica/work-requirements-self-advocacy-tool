187b5ef02ce1695402f9241d3e1675e58f51ef5a
Marc Perlman <mperlman@codeforamerica.org>
2026-05-20 18:35:09 -0500

    some cleanup

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -1,21 +1,13 @@
 class SurveyMailer < ApplicationMailer
-  default from: "noreply@" + ENV.fetch("DOMAIN", "codeforamerica.app")
-
   def send_survey(outgoing_email:)
     Rails.logger.info "Sending survey to #{outgoing_email.screener.id}"
     @screener = outgoing_email.screener
+
     state_info = LocationData::States::STATES_INFO.fetch(@screener.state)
     @survey_link = state_info.fetch(:survey_url)
 
-    attachments.inline["gbh_email_header.png"] = File.binread(Rails.root.join("app/assets/images/gbh_email_header.png"))
-
-    if ENV["SES_CONFIGURATION_SET"]
-      headers["X-SES-CONFIGURATION-SET"] = ENV["SES_CONFIGURATION_SET"]
-    end
-
-    if ENV["SES_CONTACT_LIST"]
-      headers["X-SES-LIST-MANAGEMENT-OPTIONS"] = "#{ENV["SES_CONTACT_LIST"]}; topic=general"
-    end
+    attach_header_image
+    apply_ses_headers
 
     mail(to: @screener.email, subject: I18n.t("views.survey_mailer.send_survey.subject"))
   end
--- spec/mailers/survey_mailer_spec.rb
+++ spec/mailers/survey_mailer_spec.rb
@@ -3,27 +3,26 @@ require "rails_helper"
 RSpec.describe SurveyMailer, type: :mailer do
   describe "send_survey" do
     let(:state) { LocationData::States::NORTH_CAROLINA }
+
     let(:screener) do
       create(:screener, state: state, email: "hi@example.com")
     end
+
     let(:outgoing_email) { create(:outgoing_email, screener: screener) }
-    let(:mail) do
-      SurveyMailer.send_survey(outgoing_email: outgoing_email)
-    end
-    let(:body) { mail.html_part.body.to_s }
+    let(:mail) { SurveyMailer.send_survey(outgoing_email: outgoing_email) }
+    let(:body) { html_body(mail) }
+
+    it_behaves_like "a mailer with default headers"
 
     it "renders headers and body" do
       expect(mail.subject).to eq(I18n.t("views.survey_mailer.send_survey.subject"))
-      expect(mail.from).to eq(["noreply@codeforamerica.app"])
-
-      doc = Nokogiri::HTML(body)
-      expect(doc.text).to include(I18n.t("views.survey_mailer.send_survey.greeting"))
-      expect(doc.text).to include(I18n.t("views.survey_mailer.send_survey.paragraph_2"))
+      expect(html_doc(mail).text).to include(I18n.t("views.survey_mailer.send_survey.greeting"))
+      expect(html_doc(mail).text).to include(I18n.t("views.survey_mailer.send_survey.paragraph_2"))
     end
 
     context "when state is NC" do
       it "includes NC survey link" do
-        expect(body).to include("https://codeforamerica.co1.qualtrics.com/jfe/form/SV_3kgHxveKeorfFC6")
+        expect(body).to include(LocationData::States::STATES_INFO[state][:survey_url])
         expect(body).to include("Take this 2-minute survey")
       end
     end
@@ -32,7 +31,7 @@ RSpec.describe SurveyMailer, type: :mailer do
       let(:state) { LocationData::States::DELAWARE }
 
       it "includes DE survey link" do
-        expect(body).to include("https://codeforamerica.co1.qualtrics.com/jfe/form/SV_8rdcUjhPOWA0XzM")
+        expect(body).to include(LocationData::States::STATES_INFO[state][:survey_url])
         expect(body).to include("Take this 2-minute survey")
       end
     end
@@ -43,14 +42,6 @@ RSpec.describe SurveyMailer, type: :mailer do
       it "raises an error" do
         expect { mail.deliver_now }.to raise_error(KeyError)
       end
-    end
-
-    it "attaches the inline header image" do
-      attachment = mail.attachments["gbh_email_header.png"]
-
-      expect(attachment).to be_present
-      expect(attachment.content_type).to start_with("image/png")
-      expect(attachment.body.decoded).not_to be_empty
     end
   end
 end
