59ee43b3e3ea6449fd9add5efeb9cb3956bcf1dd
Marc Perlman <mperlman@codeforamerica.org>
2026-05-20 18:05:49 -0500

    some cleanup

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -1,15 +1,11 @@
 class SurveyMailer < ApplicationMailer
   default from: "noreply@" + ENV.fetch("DOMAIN", "codeforamerica.app")
 
-  SURVEY_LINKS = {
-    LocationData::States::NORTH_CAROLINA => "https://codeforamerica.co1.qualtrics.com/jfe/form/SV_3kgHxveKeorfFC6",
-    LocationData::States::DELAWARE => "https://codeforamerica.co1.qualtrics.com/jfe/form/SV_8rdcUjhPOWA0XzM"
-  }.freeze
-
   def send_survey(outgoing_email:)
     Rails.logger.info "Sending survey to #{outgoing_email.screener.id}"
     @screener = outgoing_email.screener
-    @survey_link = SURVEY_LINKS.fetch(@screener.state)
+    state_info = LocationData::States::STATES_INFO.fetch(@screener.state)
+    @survey_link = state_info.fetch(:survey_url)
 
     attachments.inline["gbh_email_header.png"] = File.binread(Rails.root.join("app/assets/images/gbh_email_header.png"))
 
