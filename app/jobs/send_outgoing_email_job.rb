43bcccc58bb27d33270ceb7678e4816798203bea
Marc Perlman <mperlman@codeforamerica.org>
2026-06-15 16:06:30 -0500

    [WRSAT-672] Handle Aws::SESV2::Errors::AccessDeniedException from email allowlist errors better

--- app/jobs/send_outgoing_email_job.rb
+++ app/jobs/send_outgoing_email_job.rb
@@ -1,6 +1,10 @@
 class SendOutgoingEmailJob < ApplicationJob
   queue_as :default
 
+  discard_on Aws::SESV2::Errors::AccessDeniedException do |job, error|
+    Rails.logger.warn("SES access denied, discarding email job | email_id=#{job.arguments.first} | error=#{error.message}")
+  end
+
   def perform(email_id)
     outgoing_email = OutgoingEmail.find(email_id)
     ScreenerMailer.send_screener_results(outgoing_email: outgoing_email).deliver_now
