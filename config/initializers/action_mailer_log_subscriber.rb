7e46166da2bbcb5fd62e26d8006f33b08e3ae70f
Marc Perlman <mperlman@codeforamerica.org>
2026-06-16 13:45:07 -0500

    trying to tweak email logging

--- config/initializers/action_mailer_log_subscriber.rb
+++ config/initializers/action_mailer_log_subscriber.rb
@@ -3,7 +3,7 @@ ActionMailer::LogSubscriber.prepend(Module.new do
     event.payload[:to] = Array(event.payload[:to]).map { "[email redacted]" }
 
     if (exc = event.payload[:exception_object])
-      sanitized = exc.message.gsub(/[\w.+\-]+@[\w.\-]+\.\w+/, "[email redacted]")
+      sanitized = exc.message.gsub(/[\w.+-]+@[\w.-]+\.\w+/, "[email redacted]")
       if exc.is_a?(Aws::SESV2::Errors::AccessDeniedException)
         warn { "Error delivering mail #{event.payload[:message_id]}: #{sanitized}" }
         return
