8084a55d1fe804413c1e34a100397443a74b648e
Marc Perlman <mperlman@codeforamerica.org>
2026-06-16 13:47:37 -0500

    trying to tweak email logging

--- config/initializers/action_mailer_log_subscriber.rb
+++ config/initializers/action_mailer_log_subscriber.rb
@@ -1,15 +1,17 @@
-ActionMailer::LogSubscriber.prepend(Module.new do
-  def deliver(event)
-    event.payload[:to] = Array(event.payload[:to]).map { "[email redacted]" }
+ActiveSupport.on_load(:action_mailer) do
+  ActionMailer::LogSubscriber.prepend(Module.new do
+    def deliver(event)
+      event.payload[:to] = Array(event.payload[:to]).map { "[email redacted]" }
 
-    if (exc = event.payload[:exception_object])
-      sanitized = exc.message.gsub(/[\w.+-]+@[\w.-]+\.\w+/, "[email redacted]")
-      if exc.is_a?(Aws::SESV2::Errors::AccessDeniedException)
-        warn { "Error delivering mail #{event.payload[:message_id]}: #{sanitized}" }
-        return
+      if (exc = event.payload[:exception_object])
+        sanitized = exc.message.gsub(/[\w.+-]+@[\w.-]+\.\w+/, "[email redacted]")
+        if exc.is_a?(Aws::SESV2::Errors::AccessDeniedException)
+          warn { "Error delivering mail #{event.payload[:message_id]}: #{sanitized}" }
+          return
+        end
+        event.payload[:exception_object] = exc.exception(sanitized)
       end
-      event.payload[:exception_object] = exc.exception(sanitized)
+      super
     end
-    super
-  end
-end)
+  end)
+end
