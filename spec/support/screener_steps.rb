da07307d35c1bc0c2b73477c648d830447b385c0
Marc Perlman <mperlman@codeforamerica.org>
2026-06-17 17:08:51 -0500

    fixing tests

--- spec/support/screener_steps.rb
+++ spec/support/screener_steps.rb
@@ -178,7 +178,7 @@ module ScreenerSteps
     fill_in I18n.t("views.basic_info_case_number.edit.case_number_label"), with: "ABC-123"
     click_on I18n.t("general.continue")
     expect(page).to have_selector("h1", text: I18n.t("views.signature.edit.title"))
-    click_on I18n.t("general.continue")
+    click_on I18n.t("views.signature.edit.button_label")
     expect(page).to have_selector("h1", text: I18n.t("views.download_form.edit.title_sent", email: email))
   end
 
