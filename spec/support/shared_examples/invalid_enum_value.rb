fb17b7540f66a37b82c493af9b29c8fa00f9be6d
Marc Perlman <mperlman@codeforamerica.org>
2026-06-23 17:28:59 -0500

    adding NC tests

--- spec/support/shared_examples/invalid_enum_value.rb
+++ spec/support/shared_examples/invalid_enum_value.rb
@@ -1,11 +1,11 @@
-RSpec.shared_examples "rejects invalid enum values" do |fields:|
+RSpec.shared_examples "rejects invalid enum values" do |fields:, params_key: :screener, screener_factory: -> { create(:screener) }|
   fields.each do |field|
     context "with invalid value for #{field}" do
       it "returns 422 unprocessable content and does not raise" do
-        screener = create(:screener)
+        screener = instance_exec(&screener_factory)
         sign_in screener
 
-        post :update, params: {screener: {field => "invalid_enum_value"}}
+        post :update, params: {params_key => {field => "invalid_enum_value"}}
 
         expect(response).to have_http_status(:unprocessable_content)
       end
