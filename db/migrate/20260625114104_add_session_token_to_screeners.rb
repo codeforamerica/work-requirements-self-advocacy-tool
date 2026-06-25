4170c4d4f810a04a505a38f4da9ce2ca234ced47
Marc Perlman <mperlman@codeforamerica.org>
2026-06-25 12:44:31 -0500

    cleanup

--- db/migrate/20260625114104_add_session_token_to_screeners.rb
+++ db/migrate/20260625114104_add_session_token_to_screeners.rb
@@ -1,15 +1,6 @@
-class AddSessionTokenToScreeners < ActiveRecord::Migration[7.2]
-  def up
-    add_column :screeners, :session_token, :string
-
-    execute("UPDATE screeners SET session_token = md5(id::text || random()::text || extract(epoch from clock_timestamp())::text)")
-
-    change_column_null :screeners, :session_token, false
+class AddSessionTokenToScreeners < ActiveRecord::Migration[8.1]
+  def change
+    add_column :screeners, :session_token, :string, null: false, default: ""
     add_index :screeners, :session_token, unique: true
-  end
-
-  def down
-    remove_index :screeners, :session_token
-    remove_column :screeners, :session_token
   end
 end
