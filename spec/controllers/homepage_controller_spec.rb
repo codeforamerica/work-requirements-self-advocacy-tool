2e48d93f4673efc4ee0bc021db3fe0e71329987b
Anisha Ramnani <aramnani@codeforamerica.org>
2026-06-29 17:03:02 -0400

    [WRSAT-658] add CGI escape to URL encode strings to prevent error log pollution with 500 errors (#415)

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -1,6 +1,32 @@
 require "rails_helper"
 
 RSpec.describe HomepageController, type: :controller do
+  describe "#redirect_without_source" do
+    it "redirects to the base path with the source as a query param" do
+      get :redirect_without_source, params: {base_path: "en", intended_source: "fancysource"}
+
+      expect(response).to redirect_to("/en?source=fancysource")
+    end
+
+    it "redirects to root when base_path is absent" do
+      get :redirect_without_source, params: {intended_source: "fancysource"}
+
+      expect(response).to redirect_to("/?source=fancysource")
+    end
+
+    it "does not raise a 500 when intended_source contains URI-unsafe characters" do
+      get :redirect_without_source, params: {base_path: "en", intended_source: "\#{7_7}"}
+
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "does not raise a 500 when base_path contains URI-unsafe characters" do
+      get :redirect_without_source, params: {base_path: "{bad}", intended_source: "test"}
+
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
   describe "#create_screener" do
     it "creates a new screener and puts the id in the session" do
       visitor_id = "BEEPBEEPBEEP"
