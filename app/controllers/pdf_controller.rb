5116cec77209acb465b9f526555d5d7d2ba54ed6
Anisha Ramnani <anisha.ramnani@gmail.com>
2026-08-20 20:07:32 -0400

    add back in before action to skip screener and locale plus adjust css comment

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,5 +1,6 @@
 class PdfController < QuestionController
   layout "pdf"
+  skip_before_action :set_screener_current_step_and_locale
 
   def generate_pdf
     send_mixpanel_event(event_name: "pdf_downloaded")
