bd1e0d0eb2f2f9eee359aa283eaab47b406604d7
Jenny Heath <jheath@codeforamerica.org>
2026-08-27 17:16:12 -0700

    Add About page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     get "generate_pdf" => "pdf#generate_pdf"
     get "page_one_preview" => "pdf#summary_page"
     resource :privacy_policy, only: [:show], controller: "privacy_policy"
+    resource :about, only: [:show], controller: "about"
     get "clear_flashes" => "application#clear_flashes"
     get "sign_out_and_redirect" => "application#sign_out_and_redirect"
     get "/robots.txt", to: "robots#show", format: :text
